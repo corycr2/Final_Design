9cf14dfc06f60f6137af38e9a171b3ed649d0e51
Cory Lewis <77686059+corycr2@users.noreply.github.com>
2023-03-13 16:02:27 -0400

    Update square.py

--- Design/square.py
+++ Design/square.py
@@ -1,8 +1,24 @@
-def square(frequency):
-    pwm = GPIO.PWM(21, frequency) # 1 kHz frequency
+import pigpio
 
-def function_on():
-    pwm.start(50)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.OUT)
 
-def function_off():
+
+def square(squareIs, sineIs):
+    squareIs = True
+    sineIs = False
+
+def sine(sineIs, squareIs):
+    squareIs = False
+    sineIs = True
+
+def square_function_on(frequency,amplitude,squareIs, sineIs):
+    if squareIs == True:
+        pwm = GPIO.PWM(21, frequency) # 1 kHz frequency
+        pwm.start(50)
+    elif sineIs == True:
+        #do things
+        sineIs = True
+
+def function_off(pwm):
     pwm.stop()
